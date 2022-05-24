b1e91a4ab35bb04e2709cda8e48dde47bfe4405b
Paul <mets1005@yahoo.com>
2022-05-23 20:15:32 -0400

    tset

---
+++ src/main/java/com/scally/serverutils/ServerUtils.java
@@ -0,0 +1,17 @@
+package com.scally.serverutils;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ServerUtils extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
