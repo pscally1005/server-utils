af828ac971bb3cd8a24d99ecabf3b4e7617c905a
Paul <mets1005@yahoo.com>
2022-07-25 20:16:27 -0400

    added tab fill for ~

--- src/main/java/com/scally/serverutils/executors/SlabsCommandExecutor.java
+++ src/main/java/com/scally/serverutils/executors/SlabsCommandExecutor.java
@@ -15,6 +15,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
+import org.bukkit.util.RayTraceResult;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
@@ -130,8 +131,24 @@ public class SlabsCommandExecutor implements CommandExecutor, TabCompleter {
         if (!(sender instanceof Player)) {
             return Collections.EMPTY_LIST;
         }
+
         Player player = (Player) sender;
-        Block targ = player.getTargetBlock(Set.of(Material.AIR, Material.CAVE_AIR, Material.VOID_AIR, Material.WATER), 5);
+        final RayTraceResult rayTraceResult = player.rayTraceBlocks(5);
+        if (rayTraceResult == null) {
+            switch(args.length) {
+                case 1,4:
+                    return List.of("~", "~ ~", "~ ~ ~");
+                case 2,5:
+                    return List.of("~", "~ ~");
+                case 3, 6:
+                    return List.of("~");
+                case 7, 8:
+                    return onTabCompleteDistribution(args[args.length-1]);
+            }
+        }
+
+        final Block targ = rayTraceResult.getHitBlock();
+        //Block targ = player.getTargetBlock(Set.of(Material.AIR, Material.CAVE_AIR, Material.VOID_AIR, Material.WATER), 5);
         switch(args.length) {
             case 1, 4:
                 return List.of(targ.getX() + "", targ.getX() + " " + targ.getY(), targ.getX() + " " + targ.getY() + " " + targ.getZ() );
