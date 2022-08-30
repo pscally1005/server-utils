75db3d6e95b2fa185e4dc2baccc4d9481b101b83
Paul <mets1005@yahoo.com>
2022-08-29 20:29:44 -0400

    started stairsChangeset

--- src/main/java/com/scally/serverutils/stairs/StairsChange.java
+++ src/main/java/com/scally/serverutils/stairs/StairsChange.java
@@ -2,14 +2,15 @@ package com.scally.serverutils.stairs;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.Bisected;
 import org.bukkit.block.data.Directional;
 import org.bukkit.block.data.type.Stairs;
 
 public record StairsChange(Location location,
-                           Material before,
-                           Material after,
+                           Material beforeMaterial,
+                           Material afterMaterial,
                            Bisected.Half half,
-                           Directional facing,
+                           BlockFace facing,
                            Stairs.Shape shape,
                            boolean waterlogged) {}
--- src/main/java/com/scally/serverutils/stairs/StairsChangeset.java
+++ src/main/java/com/scally/serverutils/stairs/StairsChangeset.java
@@ -1,28 +1,70 @@
 package com.scally.serverutils.stairs;
 
+import com.scally.serverutils.slabs.SlabsChange;
 import com.scally.serverutils.undo.Changeset;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.data.Bisected;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.Directional;
+import org.bukkit.block.data.type.Slab;
+import org.bukkit.block.data.type.Stairs;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StairsChangeset implements Changeset<StairsChange> {
 
+    private final List<StairsChange> stairsChangeList = new ArrayList<>();
+    private boolean locked = false;
+
     @Override
     public void add(StairsChange change) {
-        // TODO: implement
+        if (locked) {
+            throw new IllegalStateException("Changeset is already locked!");
+        }
+        stairsChangeList.add(change);
     }
 
     @Override
     public boolean undo() {
-        // TODO: implement
-        return false;
+
+        for(StairsChange stairsChange : stairsChangeList) {
+
+            Location location = stairsChange.location();
+            Block block = location.getBlock();
+
+            Bisected.Half half = stairsChange.half();
+            BlockFace facing = stairsChange.facing();
+            Stairs.Shape shape = stairsChange.shape();
+            boolean isWaterlogged = stairsChange.waterlogged();
+            block.setType(stairsChange.beforeMaterial(), false);
+
+            BlockData bd = block.getBlockData();
+            Stairs stair = (Stairs) bd;
+            stair.setHalf(half);
+            stair.setFacing(facing);
+            stair.setShape(shape);
+            stair.setWaterlogged(isWaterlogged);
+
+            World world = location.getWorld();
+            world.setBlockData(stairsChange.location(), bd);
+
+        }
+
+        return true;
+
     }
 
     @Override
     public void lock() {
-        // TODO: implement
+        locked = true;
     }
 
     @Override
     public int count() {
-        // TODO: implement
-        return 0;
+        return stairsChangeList.size();
     }
 }
