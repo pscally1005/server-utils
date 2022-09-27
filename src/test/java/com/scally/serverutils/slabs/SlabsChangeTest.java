1713bd82dc16c6d7032caeb24cffa87101d7a935
spscally <scallys96@gmail.com>
2022-09-27 08:10:50 -0400

    Refactor DistributionTabCompleter functions to be smaller

--- src/test/java/com/scally/serverutils/slabs/SlabsChangeTest.java
+++ src/test/java/com/scally/serverutils/slabs/SlabsChangeTest.java
@@ -1,7 +1,12 @@
 package com.scally.serverutils.slabs;
 
+import org.junit.jupiter.api.Test;
+
 public class SlabsChangeTest {
 
     // TODO: undo tests
 
+    @Test
+    public void test() {
+    }
 }
